35ca156d83033ceaae76559bc4efd947652c5234
vaithiyanathan <vaithiyanathan30@gmail.com>
2024-06-26 13:40:17 +0530

    features and videos and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import HowItWorks from "./components/HowItWorks";
 const App = () => {
 	return (
 		<>
 			<main className='bg-black'>
-				<Navbar></Navbar>
-				<Hero></Hero>
-				<Highlights></Highlights>
+				<Navbar />
+				<Hero />
+				<Highlights />
+				<Model />
+				<Features />
+				<HowItWorks />
+				<Footer />
 			</main>
 		</>
 	);
